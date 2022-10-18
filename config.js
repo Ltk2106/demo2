439fd838447cea4dd9ad303046f2a353f562a9bb
Ltk2106 <115920256+Ltk2106@users.noreply.github.com>
2022-10-18 23:12:22 +0700

    Add files via upload
    card

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
-const nameGirl = 'Em';
-const giftUrl = 'http://nodemy.vn';
+const nameGirl = 'Ngọc Anh';
+const giftUrl = 'https://www.facebook.com/Ngocanhcrazy0212';
 const eventName = 'Chúc Mừng 20-10';
-const titleCard = 'Tặng người ấy';
-const contentCard = 'Chúc honey của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const titleCard = 'Dear Ngọc Anh';
+const contentCard = 'Nhân ngày 20/10, tớ chúc Ngọc Anh luôn vui vẻ, yêu đời, ngày càng xinh đẹp, học giỏi.Và trên hết tớ mong rằng NA sẽ luôn gặp thuận lợi trong cuộc sống,vơi đi nỗi buồn và càng nhiều niềm vui.';
 
-// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+
 // Cần hỗ trợ hãy liên hệ: 
 // Mr-Nam http://facebook.com/nam.nodemy
 // Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
-const giftImage = 'hot-girl.png';
+const giftImage = 'NA.jpg';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
